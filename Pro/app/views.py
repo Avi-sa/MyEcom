7715b27840b904e3e124d8f6153478f46577ba11
Avi-sa <avivish000@gmail.com>
2020-05-29 22:32:08 +0530

    Image

--- Pro/app/views.py
+++ Pro/app/views.py
@@ -5,4 +5,9 @@ def home(request):
     product = Product.objects.all()
     context = {'product':product}
     return render(request,'products/home.html',context)
+
+def all(request):
+    product = Product.objects.all()
+    context = {'product':product}
+    return reverse(request,'products/all.html',context)
 # Create your views here.
