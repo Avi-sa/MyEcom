363b0caa46b5210845adc18a345636b96db08b99
Akash Kumar <akashrajput2012001@gmail.com>
2020-10-01 21:03:40 +0530

    everything is working and upto date

--- Pro/Pro/urls.py
+++ Pro/Pro/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from . import settings
+from django.conf import settings
 from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('app.urls')),
+    path('shop/',include('shop.urls')),
+    path('app/',include('app.urls')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
